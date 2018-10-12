cb1879c4dabe77b7ed840c014b3496d2b493fdca
Bryan_Zhou <Bryan_Zhou@bb.com>
2018-10-12 14:56:37 +0800

    Add LineBotService and LineBotServiceTest

---
+++ app/Services/LineBotService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services;
+
+use LINE\LINEBot;
+use LINE\LINEBot\Response;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+
+
+class LineBotService
+{
+    /** @var LINEBot */
+    private $lineBot;
+    private $lineUserId;
+
+    public function __construct($lineUserId)
+    {
+        $this->lineUserId = $lineUserId;
+        $this->lineBot = app(LINEBot::class);
+    }
+
+    public function fake()
+    {
+    }
+
+    /**
+     * @param TemplateMessageBuilder|string $content
+     * @return Response
+     */
+    public function pushMessage($content): Response
+    {
+        if (is_string($content)) {
+            $content = new TextMessageBuilder($content);
+        }
+        return $this->lineBot->pushMessage($this->lineUserId, $content);
+    }
+}
