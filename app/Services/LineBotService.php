5d09fdb733ad5909cc654a345583e0a8ccce68c5
Bryan_Zhou <Bryan_Zhou@bb.com>
2018-10-15 12:27:25 +0800

    Add push ImageCarousel message with test

--- app/Services/LineBotService.php
+++ app/Services/LineBotService.php
@@ -6,6 +6,9 @@ use LINE\LINEBot;
 use LINE\LINEBot\Response;
 use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder;
 
 
 class LineBotService
@@ -34,5 +37,53 @@ class LineBotService
             $content = new TextMessageBuilder($content);
         }
         return $this->lineBot->pushMessage($this->lineUserId, $content);
+    }
+
+    public function buildTemplateMessageBuilderDeprecated(
+        string $imagePath,
+        string $directUri,
+        string $label
+    ): TemplateMessageBuilder {
+        $aa = new UriTemplateActionBuilder($label, $directUri);
+        $bb =  new ImageCarouselColumnTemplateBuilder($imagePath, $aa);
+        $target = new ImageCarouselTemplateBuilder([$bb, $bb, $bb, $bb, $bb, $bb, $bb, $bb]);
+
+        return new TemplateMessageBuilder('test123', $target);
+    }
+
+    public function buildTemplateMessageBuilder(array $data, string $notificationText = '新通知來囉!'): array
+    {
+        $imageCarouselColumnTemplateBuilders = array_map(function ($d) {
+            return $this->buildImageCarouselColumnTemplateBuilder(
+                $d['imagePath'],
+                $d['directUri'],
+                $d['label']
+            );
+        }, $data);
+
+        $tempChunk = array_chunk($imageCarouselColumnTemplateBuilders, 5);
+        return array_map(function ($data) use ($notificationText) {
+            return new TemplateMessageBuilder(
+                $notificationText,
+                new ImageCarouselTemplateBuilder($data)
+            );
+        }, $tempChunk);
+    }
+
+    /**
+     * @param string $imagePath
+     * @param string $directUri
+     * @param string $label
+     * @return ImageCarouselColumnTemplateBuilder
+     */
+    protected function buildImageCarouselColumnTemplateBuilder(
+        string $imagePath,
+        string $directUri,
+        string $label
+    ): ImageCarouselColumnTemplateBuilder {
+        return new ImageCarouselColumnTemplateBuilder(
+            $imagePath,
+            new UriTemplateActionBuilder($label, $directUri)
+        );
     }
 }
